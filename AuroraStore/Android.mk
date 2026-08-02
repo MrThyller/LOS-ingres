56951ad7118ddd6cdad9b23cd457d16a4f2bc9e9
WhoFoss <whofoss@tuta.io>
2026-08-01 22:47:53 -0300

    Update Android.mk

--- AuroraStore/Android.mk
+++ AuroraStore/Android.mk
@@ -1,10 +1,9 @@
 #
-# AuroraStore, AuroraDroid as regular system app as part of a build
+# AuroraStore as regular system app as part of a build
 # AuroraServices as privileged system app to handle the installations
 #
 # Prebuilt content taken from:
 #    https://gitlab.com/AuroraOSS/AuroraStore/tags
-#    https://gitlab.com/AuroraOSS/AuroraDroid/tags
 #    https://gitlab.com/AuroraOSS/AuroraServices/tags
 #
 LOCAL_PATH := $(call my-dir)
@@ -36,13 +35,4 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_REPLACE_PREBUILT_APK_INSTALLED := $(LOCAL_PATH)/$(LOCAL_SRC_FILES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE := AuroraDroid
-LOCAL_SRC_FILES := AuroraDroid.apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
