15df2a68b17285da83c62601a263bd0b7b3cc00d
WhoFoss <whofoss@tuta.io>
2026-08-01 21:45:49 -0300

    Create Android.mk

---
+++ AuroraStore/Android.mk
@@ -0,0 +1,48 @@
+#
+# AuroraStore, AuroraDroid as regular system app as part of a build
+# AuroraServices as privileged system app to handle the installations
+#
+# Prebuilt content taken from:
+#    https://gitlab.com/AuroraOSS/AuroraStore/tags
+#    https://gitlab.com/AuroraOSS/AuroraDroid/tags
+#    https://gitlab.com/AuroraOSS/AuroraServices/tags
+#
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp-permissions-AuroraServices.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := AuroraServices
+LOCAL_SRC_FILES := AuroraServices.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REQUIRED_MODULES := privapp-permissions-AuroraServices.xml
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := AuroraStore
+LOCAL_SRC_FILES := AuroraStore.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_REPLACE_PREBUILT_APK_INSTALLED := $(LOCAL_PATH)/$(LOCAL_SRC_FILES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := AuroraDroid
+LOCAL_SRC_FILES := AuroraDroid.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_CERTIFICATE := PRESIGNED
+include $(BUILD_PREBUILT)
